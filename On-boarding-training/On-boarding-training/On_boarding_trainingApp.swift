bceebc6a7797d5319788e6cd2eaf43f170b74e5c
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-18 20:08:31 +0200

    first push of the xcode project

---
+++ On-boarding-training/On-boarding-training/On_boarding_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_boarding_trainingApp.swift
+//  On-boarding-training
+//
+//  Created by Antonio Palomba on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct On_boarding_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
