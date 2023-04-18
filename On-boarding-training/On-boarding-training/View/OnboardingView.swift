84198892b8c034bfc9041053cd348f9864d68867
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-18 20:57:38 +0200

    splitted files inside different folders

---
+++ On-boarding-training/On-boarding-training/View/OnboardingView.swift
@@ -0,0 +1,55 @@
+//
+//  OnboardingView.swift
+//  On-boarding-training
+//
+//  Created by Antonio Palomba on 18/04/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @Binding var shouldShowOnBoarding: Bool
+    var body: some View {
+        TabView {
+            PageView(
+                title: "Push Notifications",
+                subtitle: "Enable notifications to stay up to date with friends.",
+                imageName: "bell",
+                showsDismissButton: false,
+                shouldShowOnBoarding: $shouldShowOnBoarding
+            )
+
+            PageView(
+                title: "Bookmarks",
+                subtitle: "Save your favorite pieces of content.",
+                imageName: "bookmark",
+                showsDismissButton: false,
+                shouldShowOnBoarding: $shouldShowOnBoarding
+            )
+
+            PageView(
+                title: "Flights",
+                subtitle: "Book flights to the places you want to go",
+                imageName: "airplane",
+                showsDismissButton: false,
+                shouldShowOnBoarding: $shouldShowOnBoarding
+            )
+
+            PageView(
+                title: "Home",
+                subtitle: "Go home wherever you might be",
+                imageName: "house",
+                showsDismissButton: true,
+                shouldShowOnBoarding: $shouldShowOnBoarding
+            )
+        }
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(shouldShowOnBoarding: .constant(true))
+    }
+}
